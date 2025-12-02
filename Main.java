5dc995d53e15feada2e8994e099f5418f5a3804a
Maanvi Gupta <maanvigupta133@gmail.com>
2025-12-02 14:38:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,39 @@
+package com.maanvi;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter name");
+        Student st1 = new Student();
+        st1.name = sc.nextLine();
+        st1.age = 22;
+        st1.info(st1.name, st1.age);
+        st1.info(st1.age);
+
+    }
+
+}
+
+class Student {
+
+    String name;
+    int age;
+
+    public void info(String name) {
+        System.out.println(name);
+
+    }
+
+    public void info(int age) {
+        System.out.println(age);
+    }
+
+    public void info(String name, int age) {
+        System.out.println(name + "\n" + age);
+    }
+
+}
